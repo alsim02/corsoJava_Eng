e04f8f30322992c0b39157ab1d8fee2031248602
alsim02 <Alessandro.Simeone@eng.it>
2024-11-18 17:12:31 +0100

    Update and rename Main.java to EserciziGiornalieri/Esercizio_Giorno1_0.java

--- EserciziGiornalieri/Esercizio_Giorno1_0.java
+++ EserciziGiornalieri/Esercizio_Giorno1_0.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Esercizio_Giorno1_0 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean flag = true;
